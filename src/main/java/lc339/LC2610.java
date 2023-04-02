3526525b80b7f523254360787afec5d51eef5846
BKD471 <bhaskarkumardas9@gmail.com>
2023-04-02 22:24:05 +0530

    D1

---
+++ src/main/java/lc339/LC2610.java
@@ -0,0 +1,31 @@
+package lc339;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC2610 {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        int n=nums.length;
+        int[] hash=new int[202];
+        for(int i=0;i<n;i++) {
+            hash[nums[i]]++;
+        }
+        int mx=Integer.MIN_VALUE;
+        for(int i=0;i<=201;i++){
+            mx=Math.max(mx,hash[nums[i]]);
+        }
+
+        int idx=1;
+        while(idx<mx+1){
+            List<Integer> temp=new ArrayList<>();
+            for(int i=0;i<=201;i++){
+                if(hash[i]>=idx) temp.add(i);
+            }
+
+            if(temp.size()>0) res.add(temp);
+            idx++;
+        }
+        return res;
+    }
+}
